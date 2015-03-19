1f1b3995e759bcd1aff671eb03242808401e6b7e
Hiroshi Hatake <hatake@clear-code.com>
2015-03-19 14:43:30 +0900

    Add skeleton for startup operation

---
+++ components/Startup.js
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+const kCID  = Components.ID('{B1223178-FCE2-4185-8E74-BE1DC67E1831}');
+const kID   = '@clear-code.com/tb-bug766495/startup;1';
+const kNAME = 'tbBug766495StartupService';
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
+
+function tbBug766495StartupService() {}
+
+tbBug766495StartupService.prototype = {
+  classID          : kCID,
+  contractID       : kID,
+  classDescription : kNAME,
+  observe : function(aSubject, aTopic, aData) {
+
+  },
+  QueryInterface   : XPCOMUtils.generateQI([Ci.nsIObserver]),
+}
+
+var NSGetFactory = XPCOMUtils.generateNSGetFactory([tbBug766495StartupService]);
