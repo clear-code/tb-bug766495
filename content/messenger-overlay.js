60bc858eb74db57ce4c62a68ea61b9a7ab40c297
Hiroshi Hatake <hatake@clear-code.com>
2015-03-26 14:05:51 +0900

    Get draft folders asynchronously

--- content/messenger-overlay.js
+++ content/messenger-overlay.js
@@ -8,8 +8,11 @@
 
   var tbBug766495 = {
     collectDraftFolders: function() {
+      return new Promise((function(aResolve, aReject) {
       var accountManager = MailServices.accounts;
       var allServers = accountManager.allServers;
+      var folders = [];
+      var expectedFoldersCount = 0;
       for (var i = 0,  maxi = allServers.length; i < maxi; ++i) {
         let currentServer = allServers.queryElementAt(i, Ci.nsIMsgIncomingServer);
         if (currentServer.type == "none")
@@ -19,23 +22,40 @@
         var rdf = Cc['@mozilla.org/rdf/rdf-service;1']
                     .getService(Ci.nsIRDFService);
         var folder = rdf.GetResource(folderURI).QueryInterface(Ci.nsIMsgFolder);
-        if (currentServer.type == "pop3")
-
-        if (currentServer.type == "imap") {
-          var imapDraftFolder = folder.QueryInterface(Ci.nsIMsgImapMailFolder);
-        }
+        expectedFoldersCount++;
+        this.ensureUpdated(folder).then(function(folder) {
+          folders.push(folder);
+          if (folders.length >= expectedFoldersCount) {
+            aResolve(folders);
+          }
+        });
+      }
+      if (expectedFoldersCount == 0) {
+        aResolve(folders);
+      }
+      }).bind(this));
+    },
+    ensureUpdated: function(aFolder) {
+      if (aFolder instanceof Ci.nsIMsgImapMailFolder) {
+        return new Promise((function(aResolve, aReject) {
+          var listener = this.createURLListenerForFolder(function () {
+            aResolve(aFolder);
+          });
+          var imapDraftFolder = aFolder.QueryInterface(Ci.nsIMsgImapMailFolder);
+          aFolder.updateFolderWithListener(msgWindow, listener);
+        }).bind(this));
+      } else {
+        return Promise.resolve(aFolder);
       }
     },
     // nsIUrlListener
-    createURLListenerForFolder: function createURLListenerForFolder() {
+    createURLListenerForFolder: function createURLListenerForFolder(aCallBack) {
       return {
         OnStartRunningUrl: function OnStartRunningUrl(aURL) {
         },
         OnStopRunningUrl: function OnStopRunningUrl(aURL, aExitCode) {
-          this.finished = true;
+          aCallBack();
         },
-        folder: aFolder,
-        finished: false,
       };
     }
   };
