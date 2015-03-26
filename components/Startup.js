86686ab864c28f46378c59c73128d70de6c5aed2
Hiroshi Hatake <hatake@clear-code.com>
2015-03-26 18:02:08 +0900

    Follow module name change

--- components/Startup.js
+++ components/Startup.js
@@ -9,8 +9,8 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
 XPCOMUtils.defineLazyModuleGetter(this,
-                                  "ComposeWindowWatcher",
-                                  "resource://tb-bug766495-modules/compose-window-watcher.jsm");
+                                  "AutoCompactionController",
+                                  "resource://tb-bug766495-modules/auto-compaction-controller.jsm");
 
 function tbBug766495StartupService() {}
 
@@ -19,7 +19,7 @@ tbBug766495StartupService.prototype = {
   contractID       : kID,
   classDescription : kNAME,
   observe : function(aSubject, aTopic, aData) {
-    ComposeWindowWatcher.activateAutoCompaction();
+    AutoCompactionController.activateAutoCompaction();
   },
   QueryInterface   : XPCOMUtils.generateQI([Ci.nsIObserver]),
 }
